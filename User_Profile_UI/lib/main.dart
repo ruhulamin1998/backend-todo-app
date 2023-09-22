9b6c5ee557784eb4090fd341fa102f73792e30d8
Md. Ruhul Amin <ruhulamin.raj1998@gmail.com>
2023-09-22 17:52:58 +0600

    user Profile UI

---
+++ User_Profile_UI/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:profile/profile_ui.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.cyan,
+      ),
+      home: const MyApp(),
+    ));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ProfileUI(),
+    );
+  }
+}
